a80f30602e2adc2c956bec527080fb571a7f3e1c
Turkey <turkey2349@gmail.com>
2015-08-16 23:29:56 -0400

    - Added auto version updating - Improved the leaf handling

--- src/main/java/mpaddon/events/LeavePlaceEvent.java
+++ src/main/java/mpaddon/events/LeavePlaceEvent.java
@@ -1,10 +1,12 @@
 package mpaddon.events;
 
 import mpaddon.blocks.MPABlocks;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class LeavePlaceEvent
@@ -14,48 +16,57 @@ public class LeavePlaceEvent
 	{
 		if(e.world.isRemote)
 			return;
-		if(e.action.equals(Action.RIGHT_CLICK_BLOCK))
+		if(e.getResult() != Result.DENY && e.action.equals(Action.RIGHT_CLICK_BLOCK) && e.entityPlayer.inventory.getCurrentItem() != null)
 		{
-			if(e.entityPlayer.inventory.getCurrentItem() == null)
-				return;
 			if(e.entityPlayer.inventory.getCurrentItem().getItem().equals(Item.getItemFromBlock(Blocks.leaves)) || e.entityPlayer.inventory.getCurrentItem().getItem().equals(Item.getItemFromBlock(Blocks.leaves2)))
 			{
+				Block block = e.world.getBlock(e.x, e.y, e.z);
 				boolean placed = false;
-				if(e.face == 0)
+				if(block.onBlockActivated(e.world, e.x, e.y, e.y, e.entityPlayer, e.face, 0F, 0F, 0F))
 				{
-					e.world.setBlock(e.x, e.y - 1, e.z, MPABlocks.leave);
-					placed = true;
+					e.useItem = Result.DENY;
+					e.useBlock = Result.ALLOW;
 				}
-				else if(e.face == 1)
+				else
 				{
-					e.world.setBlock(e.x, e.y + 1, e.z, MPABlocks.leave);
-					placed = true;
-				}
-				else if(e.face == 2)
-				{
-					e.world.setBlock(e.x, e.y, e.z - 1, MPABlocks.leave);
-					placed = true;
-				}
-				else if(e.face == 3)
-				{
-					e.world.setBlock(e.x, e.y, e.z + 1, MPABlocks.leave);
-					placed = true;
-				}
-				else if(e.face == 4)
-				{
-					e.world.setBlock(e.x - 1, e.y, e.z, MPABlocks.leave);
-					placed = true;
-				}
-				else if(e.face == 5)
-				{
-					e.world.setBlock(e.x + 1, e.y, e.z, MPABlocks.leave);
-					placed = true;
-				}
-				
-				if(placed)
-				{
-					e.setCanceled(true);
-					e.entityPlayer.inventory.getCurrentItem().stackSize--;
+					e.useItem = Result.ALLOW;
+
+					if(e.face == 0)
+					{
+						e.world.setBlock(e.x, e.y - 1, e.z, MPABlocks.leave);
+						placed = true;
+					}
+					else if(e.face == 1)
+					{
+						e.world.setBlock(e.x, e.y + 1, e.z, MPABlocks.leave);
+						placed = true;
+					}
+					else if(e.face == 2)
+					{
+						e.world.setBlock(e.x, e.y, e.z - 1, MPABlocks.leave);
+						placed = true;
+					}
+					else if(e.face == 3)
+					{
+						e.world.setBlock(e.x, e.y, e.z + 1, MPABlocks.leave);
+						placed = true;
+					}
+					else if(e.face == 4)
+					{
+						e.world.setBlock(e.x - 1, e.y, e.z, MPABlocks.leave);
+						placed = true;
+					}
+					else if(e.face == 5)
+					{
+						e.world.setBlock(e.x + 1, e.y, e.z, MPABlocks.leave);
+						placed = true;
+					}
+
+					if(placed)
+					{
+						e.setCanceled(true);
+						e.entityPlayer.inventory.getCurrentItem().stackSize--;
+					}
 				}
 			}
 		}
